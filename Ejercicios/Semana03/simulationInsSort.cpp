ab217fb372cdbe4c74da98575fc95d9dc69d78c8
J-Machine <jhuancaan@unsa.edu.pe>
2022-05-03 19:32:18 -0500

    InsSort simulation

---
+++ Ejercicios/Semana03/simulationInsSort.cpp
@@ -0,0 +1,43 @@
+/*
+* Realicen la simulación de Insertion-Sort en el array
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void PrintVector( vector<int> &vect)
+{
+    for (auto &i: vect)
+        cout << i << " ";
+}
+void insertionSort(vector<int> &A, int _length)
+{
+    int i, j, key, position;
+    for (j = 1; j < _length; j++) 
+    {
+        key = A[j];
+        i = j-1;
+
+        while (i >= 0 && A[i] > key)
+        {
+            A[i+1] = A[i];
+            i = i-1;
+        } 
+        A[i+1] = key;
+    }
+}
+int main ()
+{
+    vector<int> secuencia{31,41,59,26,41,58};
+    cout << "Array: ";
+    PrintVector(secuencia);
+
+    cout << "\nArray Ordenado: ";
+    insertionSort(secuencia, secuencia.size());
+    PrintVector(secuencia);
+    cout<<endl;
+    return 0;
+}
